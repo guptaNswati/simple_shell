97210f9a70cff3c893af7105b4960ef4a423b108
guptaNswati <sgupta1505@gmail.com>
2016-12-13 04:15:37 +0000

    unset env

--- env-get-set.c
+++ env-get-set.c
@@ -1,5 +1,19 @@
 #include "shell.h"
 
+int checkEnv(const char *name)
+{
+	int i;
+
+	if (name == NULL || *name == '\0')
+		return (0);
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		if (name[i] == '=')
+			return (0);
+	}
+	return (i);
+}
+
 int _strlen(const char *str)
 {
 	int len;
@@ -32,7 +46,10 @@ int _setenv(const char *name, const char *value, int overwrite)
 	char *token, *new;
 	int i, j;
 
-	new = malloc(sizeof(char) * (_strlen(name) + _strlen(value) + 2));
+	i = checkEnv(name);
+	if (i == 0)
+		return (-1);
+	new = malloc(sizeof(char) * (i + _strlen(value) + 2));
 	if (new == NULL)
 		return (-1);
 	/* copy the strings */
@@ -65,6 +82,32 @@ int _setenv(const char *name, const char *value, int overwrite)
 	*environ = NULL;
 	return (0);
 }
+
+int _unsetenv(const char *name)
+{
+	extern char **environ;
+	char **newenv;
+	int i;
+	char *token;
+
+	i = checkEnv(name);
+	if (i == 0)
+		return (-1);
+	while (*environ)
+	{
+		token = strtok(*environ, "=");
+		/* delete variable name from environment */
+		if (*token == *name)
+		{
+			for (newenv = environ; *newenv != NULL; newenv++)
+				*newenv = *(newenv + 1);
+			return (0);
+		}
+		environ++;
+	}
+/* name does not exist in the environment, function succeeds */
+	return (0);
+}
 int main(void)
 {
 	char *match, *token;
@@ -86,5 +129,6 @@ int main(void)
 		printf("could not find the variable\n");
 
 	_setenv("ZNB", "world", 1);
+	_unsetenv("ZNB");
 	return (0);
 }
