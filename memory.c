483052b9ca9daa9588ae386dd340f59a46b1466b
Philip Yoo <philipyoo10@gmail.com>
2016-12-18 06:36:06 +0000

    Remove comments, comment out main and printLL

--- memory.c
+++ memory.c
@@ -19,39 +19,26 @@ void add_mem(void **p, save_mem **head)
 		newnode->next = *head;
 		*head = newnode;
 	}
-
-	printf("New node created: %p\t%p\t%p\n", newnode, newnode->loc, *p);
 }
 
 void remove_mem(void **p, save_mem **head)
 {
 	save_mem *tmp, *hold;
 
-	printf("INSIDE REMOVE_MEM\n");
 	tmp = *head;
-	printf("\t%p\n", tmp->loc);
-
 	if (p == NULL)
 	{
 		/* remove and free all nodes */
 		while (tmp)
 		{
-			printf("start removing\n");
 			hold = tmp;
 			tmp = tmp->next;
-			printf("hold->loc: %p\n", hold->loc);
 			free(hold->loc);
 			free(hold);
-			printf("hold after free?: %p\n", hold);
-
-			printf("----end----\n");
 		}
-		printf("head: %p\n", *head);
-		printf("%p\n", (*head)->loc);
-		printf("Removed all nodes...\n");
 		*head = NULL;
 	}
-	printf("inside mid of remove_mem\n");
+
 	//remove only *p
 	hold = tmp;
 	while (tmp)
@@ -59,14 +46,9 @@ void remove_mem(void **p, save_mem **head)
 		if (tmp->loc == p)
 		{
 			if (hold == tmp)
-			{
 				*head = tmp->next;
-			}
 			else
-			{
 				hold->next = tmp->next;
-				printf("inside else, after setting\n");
-			}
 
 			free(tmp->loc);
 			free(tmp);
@@ -75,22 +57,20 @@ void remove_mem(void **p, save_mem **head)
 		hold = tmp;
 		tmp = tmp->next;
 	}
-
-	printf("Removing nodes done\n");
 }
 
-void _ref_mem(void **p, char *action, save_mem **head)
+void _ref_mem(void **p, char *action)
 {
-/*	static save_mem *head = NULL;*/
+	static save_mem *head = NULL;
 	save_mem *newnode;
 
 	if (_strcmp(action, "create") == 0)
-		add_mem(p, head);
+		add_mem(p, &head);
 	else if (_strcmp(action, "remove") == 0)
-		remove_mem(p, head);
+		remove_mem(p, &head);
 }
 
-void *_malloc(unsigned int size, save_mem **head)
+void *_malloc(unsigned int size)
 {
 	void *p;
 
@@ -99,16 +79,16 @@ void *_malloc(unsigned int size, save_mem **head)
 	if (p == NULL)
 		return (NULL);
 
-	_ref_mem(&p, "create", head);
+	_ref_mem(&p, "create");
 
 	return (p);
 }
 
-void _free(void *ptr, save_mem **head)
+void _free(void *ptr)
 {
-	_ref_mem(ptr, "remove", head);
+	_ref_mem(ptr, "remove");
 }
-
+/*
 void printLL(save_mem **head)
 {
 	save_mem *tmp;
@@ -127,8 +107,8 @@ void printLL(save_mem **head)
 	}
 
 	printf("end of printLL\n//////////\n\n");
-}
-
+	}*/
+/*
 int main(void)
 {
 	static save_mem *head = NULL;
@@ -138,7 +118,7 @@ int main(void)
 	int i, size;
 
 	size = 2;
-/*
+
 	a = _malloc(10, &head);
 	a[0] = 'a';
 	a[1] = 'b';
@@ -148,7 +128,7 @@ int main(void)
 	_free(a, &head);
 	printf("---\n");
 	printLL(&head);
-*/
+
 	for (i = 0; i < 20; i++)
 	{
 		a = _malloc(size, &head);
@@ -159,7 +139,7 @@ int main(void)
 
 	_free(NULL, &head);
 	printLL(&head);
-/*	_free(a, &head);
-	printLL(&head);*/
+
 	return (0);
 }
+*/
