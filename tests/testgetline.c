3bf6b43396d2ddd516f4b4a124c7817c374a2eb8
guptaNswati <sgupta1505@gmail.com>
2016-12-18 09:57:37 +0000

    updated task list

--- tests/testgetline.c
+++ tests/testgetline.c
@@ -2,29 +2,29 @@
 
 int main()
 {
-        char *input, **tokens;
+        char *input, **tokens, **spce;
 	int i;
-
-	/*i = _getline(&input, STDIN_FILENO);
-	_line(input); */
 
 /*	signal(SIGINT, SIG_IGN); */
 	while((i = _getline(&input, STDIN_FILENO)) != -1)
 	{
-		tokens = tokenize(input, ' ');
+		tokens = tokenize(input, ';');
 		if (tokens == NULL)
 			continue;
 		while (*tokens)
 		{
-			printf("\n[tokens] %s\n", *tokens);  
+			spce = tokenize(*tokens, ' ');
+			if (spce != NULL)
+			{
+				while (*spce)
+				{
+					printf("\n[spce] %s\n", *spce);
+					spce++;
+				}
+			}
+			printf("\n[tokens] %s\n", *tokens);
 			tokens++;
 		}
-/*			printf("\n[input] %s\n", input);
-			printf("[i] %d\n", i); */
 	}
 	return (0);
 }
-
-/**
-* run of _getline is as expected :)
-**/
