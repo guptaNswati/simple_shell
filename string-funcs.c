24ed01c2929a5c24ca9ed6fdbca5e005313f49f7
Philip Yoo <philipyoo10@gmail.com>
2016-12-19 07:47:01 +0000

    Edit _strcmp to return values based on full or partial matches

--- string-funcs.c
+++ string-funcs.c
@@ -32,8 +32,10 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
+		if (s1[i] == '\0')
+			return (1);
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return (-1);
 	}
 	return (0);
 }
