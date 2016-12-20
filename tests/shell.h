8afdc318a1108c84d6aef64a933806f87c0b9853
guptaNswati <sgupta1505@gmail.com>
2016-12-20 20:42:50 +0000

    shell.h

--- tests/shell.h
+++ tests/shell.h
@@ -13,14 +13,28 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define HSTRYLIMIT 4096
-#define BUFRSIZE 1204
+static hstory *head = NULL;
 
-typedef struct strLL
-{
-	char *str;
-	struct strLL *next;
-} strLL;
+#define HSTRYLIMIT 4096
+#define BUFRSIZE 1024
+
+#define RESET "\033[0m"
+#define BLK "\033[30m"
+#define RED "\033[31m"
+#define GRN "\033[32m"
+#define YLW "\033[33m"
+#define BLU "\033[34m"
+#define MAG "\033[35m"
+#define CYN "\033[36m"
+#define WHI "\033[37m"
+#define BBLK "\033[1m\033[30m"
+#define BRED "\033[1m\033[31m"
+#define BGRN "\033[1m\033[32m"
+#define BYLW "\033[1m\033[33m"
+#define BBLU "\033[1m\033[34m"
+#define BMAG "\033[1m\033[35m"
+#define BCYN "\033[1m\033[36m"
+#define BWHI "\033[1m\033[37m"
 
 typedef struct in_built
 {
@@ -28,12 +42,24 @@ typedef struct in_built
 	void (*func) (char **);
 } in_built;
 
-typedef struct hstory{
+typedef struct hstory
+{
         char *input;
         struct hstory *next;
 } hstory;
 
-static hstory *head = NULL;
+typedef struct help_struct
+{
+	char *cmd;
+	char *synopsis;
+	void (*func) ();
+} help_struct;
+
+typedef struct save_mem
+{
+	void *loc;
+	struct save_mem *next;
+} save_mem;
 
 int checkEnv(char *name);
 int _strlen(char *str);
@@ -42,6 +68,22 @@ int _strcmp(char *s1, char *s2);
 char **deepDupe(char **args);
 char *_strcat2(char *s1, char *s2);
 char *_strcat(char *str1, char *str2, char formatter);
+int _atoi(char *s);
+
+int add_mem(void **p, save_mem **head);
+int remove_child_mem(void **p, save_mem **head);
+int remove_mem(void **p, save_mem **head);
+int _ref_mem(void *p, char *action);
+void *_malloc(unsigned int size);
+void _free(void *ptr);
+
+void cd_data(void);
+void env_data(char *cmd);
+void exit_data(void);
+void help_data(void);
+void setenv_data(void);
+void unsetenv_data(void);
+
 void chng_dr(char **str);
 void ext(char **str);
 void hlp(char **str);
@@ -60,10 +102,9 @@ char *linep_withoutspaces(char *line);
 char **tokenize(char *lineptr, char dlimtr);
 char **split_input(char *input);
 int find_builtins(char **tokens);
-void check_path(char **tokens);
+void check_path(char **tokens, char *p);
 void excute(char **tokens);
-void promptUser();
-void prompt(void);
+void promptUser(void);
 
 
 
