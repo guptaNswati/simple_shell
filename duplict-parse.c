c72a88248dbe59d2e662822da840f3fe71fc5e00
Philip Yoo <philipyoo10@gmail.com>
2016-12-20 20:43:28 +0000

    Remove unused function parsePath

--- duplict-parse.c
+++ duplict-parse.c
@@ -1,47 +1,5 @@
 #include "shell.h"
 
-strLL *parsePath(char *path, char *delim)
-{
-	strLL *head, *tmp, *newnode;
-	char *str;
-	int i, j, d, x;
-
-	if (path == NULL)
-		return (NULL);
-
-	tmp = head;
-
-	for (i = 0, j = 0; path[i]; i++)
-	{
-		for (d = 0; delim[d]; d++)
-		{
-			if (path[i] == delim[d])
-			{
-				str = _malloc(i - j + 1);
-				for (x = 0; x < (i - j); x++)
-				{
-					str[x] = path[j + x];
-				}
-				j = i + 1;
-			}
-		}
-		newnode = _malloc(sizeof(strLL));
-		if (newnode == NULL)
-			return (NULL);
-		newnode->str = str;
-		newnode->next = NULL;
-		if (tmp == NULL)
-			tmp = newnode;
-		else
-		{
-			tmp->next = newnode;
-			tmp = tmp->next;
-		}
-	}
-
-	return (head);
-}
-
 char **deepDupe(char **args)
 {
 	char **storage;
