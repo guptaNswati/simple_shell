be5c32a228de3bd7aee2fbcce3d39d9a33f86316
Philip Yoo <philipyoo10@gmail.com>
2016-12-22 02:30:56 +0000

    Convert printf to _puts for help builtin

--- help.c
+++ help.c
@@ -2,59 +2,69 @@
 
 void cd_data(void)
 {
-	printf(BBLU"\ncd"RESET": cd [dir]\n");
-	printf("  Change the shell working directory.\n\n"
-	       "  Change the current directory to DIR. The default "
-	       "DIR is the value of the HOME shell variable.\n\n"
-	       RED"  Optional DIR inputs"RESET":\n"
-	       "  -\tchanges to previous directory.\n"
-	       "  ..\tchange to parent directory.\n"
-	       "  $HOME\tchange to home directory.\n\n");
+	_puts(BBLU"\ncd"RESET);
+	_puts(": cd [dir]\n");
+	_puts("  Change the shell working directory.\n\n");
+	_puts("  Change the current directory to DIR. The default ");
+	_puts("DIR is the value of the HOME shell variable.\n\n");
+	_puts(RED"  Optional DIR inputs"RESET);
+	_puts(":\n");
+	_puts("  -\tchanges to previous directory.\n");
+	_puts("  ..\tchange to parent directory.\n");
+	_puts("  $HOME\tchange to home directory.\n\n");
 }
 
 void env_data(char *cmd)
 {
-	printf(BBLU"\n%s"RESET": %s\n"
-	       "\tPrint all of the environment.\n\n"
-	       "\tPrint the name and value pairs for all "
-	       "environment variables.\n\n", cmd, cmd);
+	_puts(BBLU"\n");
+	_puts(cmd);
+	_puts(RESET": ");
+	_puts(cmd);
+	_puts("\n\tPrint all of the environment.\n\n");
+	_puts("\tPrint the name and value pairs for all ");
+	_puts("environment variables.\n\n");
 }
 
 void exit_data(void)
 {
-	printf(BBLU"\nexit"RESET": exit [n]\n"
-	       "\tExit the shell.\n\n"
-	       "\tExits the shell with a status of N. If N is omitted, "
-	       "the exit status is that of the last command executed.\n\n");
+	_puts(BBLU"\nexit"RESET);
+	_puts(": exit [n]\n");
+	_puts("\tExit the shell.\n\n");
+	_puts("\tExits the shell with a status of N. If N is omitted, ");
+	_puts("the exit status is that of the last command executed.\n\n");
 }
 
 void help_data(void)
 {
-	printf(BBLU"\nhelp:"RESET" help [pattern ...]\n"
-	       "  Display information about builtin commands.\n\n"
-	       "  Displays brief summaries of builtin commands. "
-	       "  If PATTERN is specified, gives detailed help on all "
-	       "commands matching PATTERN, otherwise the list of "
-	       "help topics is printed.\n\n"
-	       RED"  Arguments"RESET":\n"
-	       "  PATTERN\tPattern is always compared from left to "
-	       "right, looking at each letters that match. "
-	       "If User passes 'p', all help builtin commands "
-	       "starting with 'p' will be printed.\n\n");
+	_puts(BBLU"\nhelp:"RESET);
+	_puts(" help [pattern ...]\n");
+	_puts("  Display information about builtin commands.\n\n");
+	_puts("  Displays brief summaries of builtin commands. ");
+	_puts("  If PATTERN is specified, gives detailed help on all ");
+	_puts("commands matching PATTERN, otherwise the list of ");
+	_puts("help topics is printed.\n\n");
+	_puts(RED"  Arguments"RESET);
+	_puts(":\n");
+	_puts("  PATTERN\tPattern is always compared from left to ");
+	_puts("right, looking at each letters that match. ");
+	_puts("If User passes 'p', all help builtin commands ");
+	_puts("starting with 'p' will be printed.\n\n");
 }
 
 void setenv_data(void)
 {
-	printf(BBLU"\nsetenv"RESET": setenv name value\n"
-	       "  Add an environment variable.\n\n"
-	       "  Adds the environement variable NAME to the environment. "
-	       "The value of NAME is set to VALUE.\n\n");
+	_puts(BBLU"\nsetenv"RESET);
+	_puts(": setenv name value\n");
+	_puts("  Add an environment variable.\n\n");
+	_puts("  Adds the environement variable NAME to the environment. ");
+	_puts("The value of NAME is set to VALUE.\n\n");
 }
 
 void unsetenv_data(void)
 {
-	printf(BBLU"\nunsetenv"RESET": unsetenv name\n"
-	       "  Remove an environment variable.\n\n"
-	       "  Removes the environment variable NAME from the "
-	       "environment.\n\n");
+	_puts(BBLU"\nunsetenv"RESET);
+	_puts(": unsetenv name\n");
+	_puts("  Remove an environment variable.\n\n");
+	_puts("  Removes the environment variable NAME from the ");
+	_puts("environment.\n\n");
 }
